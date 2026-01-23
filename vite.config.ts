48ac24279abc4f286ab2d687a45348bc92244b9c
Joseelv_ <jose.venegasan@gmail.com>
2026-01-23 20:28:49 +0100

    fix: corregir la ruta base en la configuración de Vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Joseeelv.github.io/',
+  base: '/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
